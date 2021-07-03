0e3768a85a1c11a6003884bd0395ba4bd32921bc
Mahedi Hassan <mahadihassan.mhd@gmail.com>
2021-07-03 22:11:03 +0600

    Final Update Push

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,9 +30,66 @@ $(document).ready(function(){
               slidesToScroll: 1
             }
           }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
         ]
       });
+      // sticky menu 
+    var navoff = $(".header_navbar").offset().top;
+
+    $(window).scroll(function () {
+        var scrolling = $(this).scrollTop();
+
+        if (scrolling > navoff) {
+            $(".header_navbar").addClass("sticky_menu");
+        } else {
+            $(".header_navbar").removeClass("sticky_menu");
+        }
+    });
+
+    // Trustpilot Review slide
+    $('.trustpilot_slide_row').slick({
+      dots: false,
+      arrows: true,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      nextArrow: '<i class="fal fa-angle-right trustpilot_arrow trustpilot_arrow_next"></i>',
+      prevArrow: '<i class="fal fa-angle-left trustpilot_arrow trustpilot_arrow_prev"></i>',
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            dots: false,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        
+      ]
+    });
 })
